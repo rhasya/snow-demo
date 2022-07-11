25837a8aa444e14f248c15e2fe436b36b8e3581a
SUNHO LEE <rhasya@gmail.com>
2022-07-12 00:18:57 +0900

    remove client folder

--- client/src/TempData.js
+++
@@ -1,8 +0,0 @@
-const problems = [
-  {id: 1, title: 'A+B', content: '두 수 A와 B를 입력받아 A+B를 출력하는 프로그램을 작성하시오.<br><h4>입력</h4><br><pre>1 2</pre><br><h4>출력</h4><br><pre>3</pre>'},
-  {id: 2, title: '문제2', content: '문제2의 내용'},
-  {id: 3, title: '문제3', content: '문제3의 내용'},
-  {id: 4, title: '문제4', content: '문제4의 내용'},
-];
-
-export { problems };
--- client/src/pages/Home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-      <main id="home" className="container pt-2">
-        <h1>처음</h1>
-      </main>
-  )
-}
-
-export default Home;
